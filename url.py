81c3d2e5347d61076aabb1fb16b74c73562981e3
upadhyay20 <129301047+upadhyay20@users.noreply.github.com>
2023-07-19 20:32:24 +0530

    Update url.py

--- url.py
+++ url.py
@@ -2,10 +2,9 @@ import pyshorteners
 
 url = "https://example.com"
 
-# Create an instance of the Shortener class
 shortener = pyshorteners.Shortener()
 
-# Shorten the URL
+
 short_url = shortener.tinyurl.short(url)
 
 print("Shortened URL:", short_url)
